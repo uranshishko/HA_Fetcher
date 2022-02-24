b4ca174365d2df0ad10d79aa94bc25e85b2d57c3
unknown <uraneshishko@gmail.com>
2022-02-24 17:11:22 +0100

    restart

--- src/index.js
+++ src/index.js
@@ -13,31 +13,38 @@ app.get("/:username", (req, res) => {
   const username = req.params.username;
   const query = req.query.data;
 
-  https.get(
-    "https://grin.co/wp-admin/admin-ajax.php?action=imc_engagement&imc_url=https://instagram.com/" +
-      username,
-    (response) => {
-      var body = "";
+  https
+    .get(
+      "https://grin.co/wp-admin/admin-ajax.php?action=imc_engagement&imc_url=https://instagram.com/" +
+        username,
+      (response) => {
+        var body = "";
 
-      response.on("data", (data) => {
-        body += data;
-      });
+        response.on("data", (data) => {
+          body += data;
+        });
 
-      response.on("end", () => {
-        let data = JSON.parse(body);
+        response.on("end", () => {
+          let data = JSON.parse(body);
 
-        if (query) {
-          let extractedData = data["user_profile"]
-            ? data["user_profile"][query]
-            : null;
-          data = {};
-          data[query] = extractedData;
-        }
+          if (query) {
+            let extractedData = data["user_profile"]
+              ? data["user_profile"][query]
+              : null;
+            data = {};
+            data[query] = extractedData;
+          }
 
-        res.json(data);
-      });
-    }
-  );
+          res.json(data);
+        });
+      }
+    )
+    .on("error", () =>
+      res.json({
+        statusCode: 500,
+        message: "Something went wrong, please try again later",
+      })
+    );
 });
 
 app.use("*", (req, res) =>
@@ -53,4 +60,4 @@ function haltOnTimeout(req, res, next) {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log("Server is upp on port " + port));
+app.listen(port, () => console.log("Server is up on port " + port));
